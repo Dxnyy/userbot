916f02ab9f00fa003fe8734e769f850afc5b1972
reedniv <26852596+reedniv@users.noreply.github.com>
2019-12-28 14:50:43 +0700

    Update chat.py

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -31,9 +31,9 @@ async def useridgetter(target):
             name, user_id))
 
 
-@register(outgoing=True, pattern="^.link(?: |$)(.*)")
+@register(outgoing=True, pattern="^.user(?: |$)(.*)")
 async def permalink(mention):
-    """ For .link command, generates a link to the user's PM with a custom text. """
+    """ For .user command, generates a link to the user's PM with a custom text. """
     user, custom = await get_user_from_event(mention)
     if not user:
         return
@@ -168,7 +168,7 @@ CMD_HELP.update({
 \nUsage: Unmutes a muted chat.\
 \n\n.mutechat\
 \nUsage: Allows you to mute any chat.\
-\n\n.link <username/userid> : <optional text> (or) reply to someone's message with .link <optional text>\
+\n\n.user <username/userid> : <optional text> (or) reply to someone's message with .user <optional text>\
 \nUsage: Generate a permanent link to the user's profile with optional custom text.\
 \n\n.regexninja on/off\
 \nUsage: Globally enable/disables the regex ninja module.\
